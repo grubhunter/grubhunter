62c2f57678529e7e6f99e86c2b56cc6832d2f9fb
Suhas Manjunath <sm5907@nyu.edu>
2016-04-07 18:05:19 -0400

    Login screen->Registration

--- grubhunter-mob/app/src/main/java/edu/nyu/gh/grubhunter_mob/Home.java
+++ grubhunter-mob/app/src/main/java/edu/nyu/gh/grubhunter_mob/Home.java
@@ -1,7 +1,10 @@
 package edu.nyu.gh.grubhunter_mob;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Home extends AppCompatActivity {
 
@@ -9,5 +12,13 @@ public class Home extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        final Button button = (Button) findViewById(R.id.textView);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Perform action on click
+                Intent i = new Intent(getApplicationContext(),Registration_page.class);
+                startActivity(i);
+            }
+        });
     }
 }
