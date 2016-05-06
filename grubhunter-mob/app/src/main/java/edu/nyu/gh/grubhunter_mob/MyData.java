8ea656a73c98fc2c90261324893c7aa8ef084b4b
shirleym <a.shirley.m@gmail.com>
2016-05-06 18:39:42 -0400

    test

--- grubhunter-mob/app/src/main/java/edu/nyu/gh/grubhunter_mob/MyData.java
+++ grubhunter-mob/app/src/main/java/edu/nyu/gh/grubhunter_mob/MyData.java
@@ -5,16 +5,25 @@ package edu.nyu.gh.grubhunter_mob;
  */
 public class MyData {
     public String myTitle;
-    public int myNum;
+    public String myNum;
     public String myRating;
+    public String landmark;
+    public String priceRange;
+    public String noReviews;
+    public String dishes;
+
     public MyData(){
         super();
     }
 
-    public MyData(String myTitle, int myNum, String myRating) {
+    public MyData(String myTitle, String myNum, String myRating, String landmark, String priceRange, String noReviews, String dishes) {
         super();
         this.myTitle = myTitle;
         this.myNum = myNum;
         this.myRating = myRating;
+        this.landmark = landmark;
+        this.priceRange = priceRange;
+        this.noReviews = noReviews;
+        this.dishes = dishes;
     }
 }
