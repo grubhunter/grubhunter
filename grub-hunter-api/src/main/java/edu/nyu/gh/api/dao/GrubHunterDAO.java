22c60d5781d40cb6de4fc271860e62555bf49446
Richie Johnson <richie@172-16-20-160.DYNAPOOL.NYU.EDU>
2016-05-11 21:06:48 -0400

    user rating generator in data

--- grub-hunter-api/src/main/java/edu/nyu/gh/api/dao/GrubHunterDAO.java
+++ grub-hunter-api/src/main/java/edu/nyu/gh/api/dao/GrubHunterDAO.java
@@ -5,6 +5,12 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import edu.nyu.gh.api.model.Dish;
+import edu.nyu.gh.api.model.GrubSimpleResponse;
+import edu.nyu.gh.api.model.Recommendation;
 
 public class GrubHunterDAO {
 
@@ -30,29 +36,72 @@ public class GrubHunterDAO {
 		return null;
 	}
 
-	public ResultSet fetchUser(String userId) throws SQLException {
-		Connection con = getRemoteConnection();
-		PreparedStatement pstmnt = con.prepareStatement("SELECT * FROM user where email=?");
-		pstmnt.setString(1, userId);
-		return pstmnt.executeQuery();
-	}
-	
-	public ResultSet fetchDishes() throws SQLException {
-		Connection con = getRemoteConnection();
-		PreparedStatement pstmnt = con.prepareStatement("SELECT dish_id,dish_name FROM dishes_available");
-		return pstmnt.executeQuery();
+	public GrubSimpleResponse fetchUser(String userId, String password) {
+		Connection con = null;
+		try {
+			con = getRemoteConnection();
+			PreparedStatement pstmnt = con.prepareStatement("SELECT * FROM user where email=?");
+			pstmnt.setString(1, userId);
+			ResultSet rs = pstmnt.executeQuery();
+
+			if (rs.next()) {
+				String pass = rs.getString(2);
+				if (pass.equals(password)) {
+					return new GrubSimpleResponse("success");
+				} else {
+					return new GrubSimpleResponse("credential error");
+				}
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				con.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+		return new GrubSimpleResponse("credential error");
 	}
 
-	public void registerUser(String email, String password, String fname, String lname, String phone, int[] dishPrefs) throws SQLException {
+	public List<Dish> fetchDishes() {
+		Connection con = null;
+		List<Dish> dishes = new ArrayList<>();
+		try {
+			con = getRemoteConnection();
+			PreparedStatement pstmnt = con.prepareStatement("SELECT dish_id,dish_name FROM dishes_available");
+			ResultSet rs = pstmnt.executeQuery();
+			while (rs.next()) {
+				Dish dish = new Dish();
+				dish.setDishId(rs.getInt(1));
+				dish.setDishName(rs.getString(2));
+				dish.setRating("");
+				dishes.add(dish);
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				con.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+		return dishes;
+	}
+
+	public void registerUser(String email, String password, String fname, String lname, String phone, int[] dishPrefs)
+			throws SQLException {
 		Connection con = getRemoteConnection();
-		PreparedStatement pstmnt = con.prepareStatement("INSERT INTO user (email,password,first_name,last_name,phone) values (?,?,?,?,?)");
+		PreparedStatement pstmnt = con
+				.prepareStatement("INSERT INTO user (email,password,first_name,last_name,phone) values (?,?,?,?,?)");
 		pstmnt.setString(1, email);
 		pstmnt.setString(2, password);
 		pstmnt.setString(3, fname);
 		pstmnt.setString(4, lname);
 		pstmnt.setString(5, phone);
 		pstmnt.executeUpdate();
-		for(int pref:dishPrefs){
+		for (int pref : dishPrefs) {
 			pstmnt = con.prepareStatement("INSERT INTO dish_preferences (email,dish_id) values (?,?)");
 			pstmnt.setString(1, email);
 			pstmnt.setInt(2, pref);
@@ -60,19 +109,19 @@ public class GrubHunterDAO {
 		}
 		pstmnt = con.prepareStatement("INSERT INTO user_recommendations (email,restaurant_id,dish_id) "
 				+ "SELECT ?,restaurant_id,dish_id from restaurant_dishes "
-				+ "where dish_id in (SELECT dish_id from dish_preferences "
-				+ "where email=?)");
+				+ "where dish_id in (SELECT dish_id from dish_preferences " + "where email=?)");
 		pstmnt.setString(1, email);
 		pstmnt.setString(2, email);
 		pstmnt.executeUpdate();
+		con.close();
 	}
-	
-	public void updatePreferences(String email,int[] dishPrefs) throws SQLException {
+
+	public void updatePreferences(String email, int[] dishPrefs) throws SQLException {
 		Connection con = getRemoteConnection();
 		PreparedStatement pstmnt = con.prepareStatement("DELETE FROM dish_preferences WHERE email=?");
 		pstmnt.setString(1, email);
 		pstmnt.executeUpdate();
-		for(int pref:dishPrefs){
+		for (int pref : dishPrefs) {
 			pstmnt = con.prepareStatement("INSERT INTO dish_preferences (email,dish_id) values (?,?)");
 			pstmnt.setString(1, email);
 			pstmnt.setInt(2, pref);
@@ -83,39 +132,83 @@ public class GrubHunterDAO {
 		pstmnt.executeUpdate();
 		pstmnt = con.prepareStatement("INSERT INTO user_recommendations (email,restaurant_id,dish_id) "
 				+ "SELECT ?,restaurant_id,dish_id from restaurant_dishes "
-				+ "where dish_id in (SELECT dish_id from dish_preferences "
-				+ "where email=?)");
+				+ "where dish_id in (SELECT dish_id from dish_preferences " + "where email=?)");
 		pstmnt.setString(1, email);
 		pstmnt.setString(2, email);
 		pstmnt.executeUpdate();
+		con.close();
 	}
-	
-	public void insertRatings(String email,int restaurantId, int dishId, String rating) throws SQLException {
+
+	public void insertRatings(String email, int restaurantId, int dishId, String rating) throws SQLException {
 		Connection con = getRemoteConnection();
-		PreparedStatement pstmnt = con.prepareStatement("DELETE FROM user_rating where email=? AND restaurant_id=? AND dish_id=?");
+		PreparedStatement pstmnt = con
+				.prepareStatement("DELETE FROM user_rating where email=? AND restaurant_id=? AND dish_id=?");
 		pstmnt.setString(1, email);
 		pstmnt.setInt(2, restaurantId);
 		pstmnt.setInt(3, dishId);
 		pstmnt.executeUpdate();
-		pstmnt = con.prepareStatement("INSERT INTO user_rating (email,restaurant_id, dish_id, dish_rating) values (?,?,?,?)");
+		pstmnt = con.prepareStatement(
+				"INSERT INTO user_rating (email,restaurant_id, dish_id, dish_rating) values (?,?,?,?)");
 		pstmnt.setString(1, email);
 		pstmnt.setInt(2, restaurantId);
 		pstmnt.setInt(3, dishId);
 		pstmnt.setString(4, rating);
 		pstmnt.executeUpdate();
+		con.close();
 	}
-	
-	public ResultSet fetchUserRecommendations(String userId) throws SQLException {
+
+	public List<Recommendation> fetchUserRecommendations(String userId) {
 		Connection con = getRemoteConnection();
-		PreparedStatement pstmnt = con.prepareStatement("SELECT email, user_recommendations.restaurant_id,name,near,address, "
-				+ "price, reviews, rating, user_recommendations.dish_id, dish_name, dish_rating "
-				+ "from user_recommendations join restaurant on(id=restaurant_id) "
-				+ "join dishes_available on(dishes_available.dish_id=user_recommendations.dish_id) "
-				+ "join restaurant_dishes on(restaurant_dishes.dish_id=user_recommendations.dish_id "
-				+ "and restaurant_dishes.restaurant_id=user_recommendations.restaurant_id) "
-				+ "where email=? order by user_recommendations.restaurant_id asc LIMIT 50");
-		pstmnt.setString(1, userId);
-		return pstmnt.executeQuery();
+		List<Recommendation> recommendations = new ArrayList<>();
+		try {
+			PreparedStatement pstmnt = con
+					.prepareStatement("SELECT email, user_recommendations.restaurant_id,name,near,address, "
+							+ "price, reviews, rating, user_recommendations.dish_id, dish_name, dish_rating "
+							+ "from user_recommendations join restaurant on(id=restaurant_id) "
+							+ "join dishes_available on(dishes_available.dish_id=user_recommendations.dish_id) "
+							+ "join restaurant_dishes on(restaurant_dishes.dish_id=user_recommendations.dish_id "
+							+ "and restaurant_dishes.restaurant_id=user_recommendations.restaurant_id) "
+							+ "where email=? order by user_recommendations.restaurant_id asc LIMIT 50");
+			pstmnt.setString(1, userId);
+			ResultSet rs = pstmnt.executeQuery();
+			int prev = 0;
+
+			Recommendation rec = null;
+			List<Dish> dishes = null;
+			while (rs.next()) {
+				int id = rs.getInt(2);
+				if (prev != id) {
+					if (rec != null) {
+						recommendations.add(rec);
+					}
+					rec = new Recommendation();
+					rec.setRestaurantId(id);
+					rec.setRestaurantName(rs.getString(3));
+					rec.setRestaurantNear(rs.getString(4));
+					rec.setAddress(rs.getString(5));
+					rec.setPrice(rs.getString(6));
+					rec.setReviews(rs.getString(7));
+					rec.setRating(rs.getString(8));
+					dishes = new ArrayList<>();
+					prev = id;
+				}
+				Dish dish = new Dish();
+				dish.setDishId(rs.getInt(9));
+				dish.setDishName(rs.getString(10));
+				dish.setRating(rs.getString(11));
+				dishes.add(dish);
+				rec.setDishes(dishes);
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				con.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+		return recommendations;
 	}
 
 }
--- grub-hunter-api/src/main/java/edu/nyu/gh/api/service/GrubHunterService.java
+++ grub-hunter-api/src/main/java/edu/nyu/gh/api/service/GrubHunterService.java
@@ -1,8 +1,6 @@
 package edu.nyu.gh.api.service;
 
-import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.ArrayList;
 import java.util.List;
 
 import javax.ws.rs.Consumes;
@@ -26,84 +24,55 @@ import edu.nyu.gh.api.model.UpdateRatingsRequest;
 @Path("/api")
 public class GrubHunterService {
 	/**
-	 * service - login service
-	 * service - user registration service
-	 * service - user preference update service
-	 * service - fetch user recommendation service
-	 * service - user update review status
-	 * service - get available dishes
-	 * */
+	 * service - login service service - user registration service service -
+	 * user preference update service service - fetch user recommendation
+	 * service service - user update review status service - get available
+	 * dishes
+	 */
 
 	@POST
 	@Consumes(MediaType.APPLICATION_JSON)
 	@Produces(MediaType.APPLICATION_JSON)
 	@Path("/login")
-	public GrubSimpleResponse login(AuthRequest request){
-		System.out.println("Got login>>"+request.getUserId()+"<>"+request.getPassword());
-		try {
-			ResultSet rs = new GrubHunterDAO().fetchUser(request.getUserId());
-			if(rs.next()){
-				String pass = rs.getString(2);
-				if (pass.equals(request.getPassword())){
-					return new GrubSimpleResponse("success");
-				}else{
-					return new GrubSimpleResponse("credential error");
-				}
-			}
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
-		return new GrubSimpleResponse("failure");
+	public GrubSimpleResponse login(AuthRequest request) {
+		System.out.println("Got login>>" + request.getUserId() + "<>" + request.getPassword());
+		return new GrubHunterDAO().fetchUser(request.getUserId(), request.getPassword());
 	}
-	
+
 	@POST
 	@Consumes(MediaType.APPLICATION_JSON)
 	@Produces(MediaType.APPLICATION_JSON)
 	@Path("/available-dishes")
-	public List<Dish> fetchDishes(AuthRequest request){
+	public List<Dish> fetchDishes(AuthRequest request) {
 		System.out.println("Got fetch dishes>>");
-		List<Dish> dishes = new ArrayList<>();
-		try {
-			ResultSet rs = new GrubHunterDAO().fetchDishes();
-			while(rs.next()){
-				Dish dish = new Dish();
-				dish.setDishId(rs.getInt(1));
-				dish.setDishName(rs.getString(2));
-				dishes.add(dish);
-			}
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
-		return dishes;
+		return new GrubHunterDAO().fetchDishes();
 	}
-	
+
 	@POST
 	@Consumes(MediaType.APPLICATION_JSON)
 	@Produces(MediaType.APPLICATION_JSON)
 	@Path("/register")
-	public GrubSimpleResponse register(RegistrationRequest request){
-		System.out.println("Got registration>>"+request.getEmail());
+	public GrubSimpleResponse register(RegistrationRequest request) {
+		System.out.println("Got registration>>" + request.getEmail());
 		try {
-			new GrubHunterDAO().registerUser(request.getEmail(), request.getPassword(), 
-					request.getFirstName(), request.getLastName(), request.getPhone(), request.getCategories());
+			new GrubHunterDAO().registerUser(request.getEmail(), request.getPassword(), request.getFirstName(),
+					request.getLastName(), request.getPhone(), request.getCategories());
 			return new GrubSimpleResponse("success");
 		} catch (MySQLIntegrityConstraintViolationException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 			return new GrubSimpleResponse("already registered");
 		} catch (SQLException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		return new GrubSimpleResponse("failure");
 	}
-	
+
 	@POST
 	@Consumes(MediaType.APPLICATION_JSON)
 	@Produces(MediaType.APPLICATION_JSON)
 	@Path("/update-preference")
-	public GrubSimpleResponse updatePreferences(UpdatePreferenceRequest request){
-		System.out.println("Got update prefs>>"+request.getEmail());
+	public GrubSimpleResponse updatePreferences(UpdatePreferenceRequest request) {
+		System.out.println("Got update prefs>>" + request.getEmail());
 		try {
 			new GrubHunterDAO().updatePreferences(request.getEmail(), request.getCategories());
 			return new GrubSimpleResponse("success");
@@ -117,66 +86,30 @@ public class GrubHunterService {
 		}
 		return new GrubSimpleResponse("failure");
 	}
-	
+
 	@POST
 	@Consumes(MediaType.APPLICATION_JSON)
 	@Produces(MediaType.APPLICATION_JSON)
 	@Path("/recommendations")
-	public List<Recommendation> fetchRecommendations(GrubSimpleRequest request){
-		System.out.println("Got recommendation request>>"+request.getUserId());
-		List<Recommendation> recommendations = new ArrayList<>();
-		try {
-			ResultSet rs = new GrubHunterDAO().fetchUserRecommendations(request.getUserId());
-			int prev = 0;
-			
-			Recommendation rec = null;
-			List<Dish> dishes = null;
-			while(rs.next()){
-				int id = rs.getInt(2);
-				if(prev != id){
-					if(rec != null){
-						recommendations.add(rec);
-					}
-					rec = new Recommendation();
-					rec.setRestaurantId(id);
-					rec.setRestaurantName(rs.getString(3));
-					rec.setRestaurantNear(rs.getString(4));
-					rec.setAddress(rs.getString(5));
-					rec.setPrice(rs.getString(6));
-					rec.setReviews(rs.getString(7));
-					rec.setRating(rs.getString(8));
-					dishes = new ArrayList<>();
-					prev = id;
-				}
-				Dish dish = new Dish();
-				dish.setDishId(rs.getInt(9));
-				dish.setDishName(rs.getString(10));
-				dish.setRating(rs.getString(11));
-				dishes.add(dish);
-				rec.setDishes(dishes);
-			}
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
-		return recommendations;
+	public List<Recommendation> fetchRecommendations(GrubSimpleRequest request) {
+		System.out.println("Got recommendation request>>" + request.getUserId());
+		return new GrubHunterDAO().fetchUserRecommendations(request.getUserId());
 	}
-	
+
 	@POST
 	@Consumes(MediaType.APPLICATION_JSON)
 	@Produces(MediaType.APPLICATION_JSON)
 	@Path("/rate-dish")
-	public GrubSimpleResponse rateDish(UpdateRatingsRequest request){
-		System.out.println("Got update ratings>>"+request.getEmail());
+	public GrubSimpleResponse rateDish(UpdateRatingsRequest request) {
+		System.out.println("Got update ratings>>" + request.getEmail());
 		try {
-			new GrubHunterDAO().insertRatings(request.getEmail(), request.getRestaurantId(),
-					request.getDishId(), request.getRating());
+			new GrubHunterDAO().insertRatings(request.getEmail(), request.getRestaurantId(), request.getDishId(),
+					request.getRating());
 			return new GrubSimpleResponse("success");
 		} catch (MySQLIntegrityConstraintViolationException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 			return new GrubSimpleResponse("duplicate rating for dish");
 		} catch (SQLException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		return new GrubSimpleResponse("failure");
